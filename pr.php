ac6a00b0a37125e28f4fb395da2fc5bf362aba6e
Kevin-S-H <kevinheckard@gmail.com>
2014-06-05 14:20:40 -0700

    new page for new website

---
+++ pr.php
@@ -0,0 +1,134 @@
+<html>
+<head>
+<title>Public Relations</title>
+<link type="text/css" rel="stylesheet" href="/Styles/style.css"></link>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript" src="/Scripts/script.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<body onLoad="init()">
+<div id="wrapper">
+<div id="banner">
+	<a href="/index.php"><img src="/Images/Banner/banner.jpg"/></a><br/>
+</div><!--banner-->
+<div id="aside">
+	<div id="nav">
+		<ul>
+			<li id="NPR"><a href="/Links/pr.php"><img src="/Images/Navigation/pr.png" width="110px"/></a></li>
+			<li id="NBD"><a href="/Links/build.php"><img src="/Images/Navigation/build.png" width="110px"/></a></li>
+			<li id="NPG"><a href="/Links/programming.php"><img src="/Images/Navigation/programming.png" width="110px"/></a></li>
+			<li id="NMD"><a href="/Links/media.php"><img src="/Images/Navigation/media.png" width="110px"/></a></li>
+			<li id="NSP"><a href="/Links/sponsors.php"><img src="/Images/Navigation/sponsors.png" width="110px"/></a></li>
+			<li id="NCT"><a href="/Links/contact us.php"><img src="/Images/Navigation/contact.png" width="110px"/></a></li>
+		</ul>
+	</div><!--nav-->
+	<div id="content">
+<h1 id="PR">Public Relations</h1>
+	
+	<h2>About the team</h2>
+	<p>The SOTABots are a local robotics team combining students from Tacoma School of the Arts, Tacoma Science and Math Institute, and Stewart Middle School. Our team is a part of the FIRST Robotics organization, a worldwide association designed to get young adults involved in engineering. Every year, we design and build several robots to compete in various FIRST challenges.</p>
+	<h2>Tacoma Robtics Alliance</h2>
+	<p>The TRA is an organization that provides support, and builds community for FIRST teams in the Puget Sound region. It was founded by SOTA and SAMI students whom make up the SOTABots. Yearly, we construct a full size practice field for every FRC and FTC seasons game challenge. The practice field we construct is open to any FIRST team who can come to our location in South Tacoma. Our Practice field enables FRC and FTC team to test and practice with their FIRST robots so they can succeed in competition. </p>
+	<p><center>In the TRA we don’t just build robots, we build a community</center></p>
+	<h2>Mentor Roster</h2>
+	<div class="name">Clifford Holmgren</div>
+	<div class="name">Dafydd "Bishop" Rhys-Jones</div>
+	<div class="name">David Olive</div>
+	<div class="name">Emmanuel Grillos</div>
+	<div class="name">James Nelson</div>
+	<div class="name">Jennifer Goetting</div>
+	<div class="name">Jon Brandon</div>
+	<div class="name">Ken Luthy(Lead)</div>
+	<div class="name">Mary Lewis</div>
+	<div class="name">Parker Olive</div>
+	<div class="name">Rich Williams(Lead)</div>
+	<div class="name">Robert Faulk</div>
+	<div class="name">Robert Girvin</div>
+	<div class="name">Robert Kroker</div>
+	<div class="name">Sara Siemens-Luthy</div>
+	<div class="name">Tina Baril</div>
+	<div class="name">Tony Duchesne</div>
+	<div style="clear:both"></div>
+
+	<h2>Student Roster</h2>
+	<div class="name">Aidan Koenig</div>
+	<div class="name">Alex Norwood</div>
+	<div class="name">Antonio Ramirez</div>
+	<div class="name">Avery Reich-Norris</div>
+	<div class="name">Boone Ferry</div>
+	<div class="name">Brian Villahermosa</div>
+	<div class="name">Cole Peterson</div>
+	<div class="name">Douglas Mitchell</div>
+	<div class="name">Ea Kirkland-Woodward</div>
+	<div class="name">Elena Meyerson</div>
+	<div class="name">Eli Gurel</div>
+	<div class="name">Elijah Williams</div>
+	<div class="name">Espen Storjell</div>
+	<div class="name">Frankie Lyon(Captain)</div>
+	<div class="name">Gabriel Reich-Norris</div>
+	<div class="name">Ian McGee</div>
+	<div class="name">Isaiah Taylor</div>
+	<div class="name">Jake Coyne</div>
+	<div class="name">Justice Greer</div>
+	<div class="name">Kevin Heckerd</div>
+	<div class="name">Kirk Foreman</div>
+	<div class="name">Lauren Haigh</div>
+	<div class="name">Lucas Grillos</div>
+	<div class="name">Maddie Siler</div>
+	<div class="name">Matt Nelson</div>
+	<div class="name">Maxx King(Captain)</div>
+	<div class="name">Maggie Rosner</div>
+	<div class="name">Maya Kalsta</div>
+	<div class="name">Oscar Lewis</div>
+	<div class="name">Preston Holmgren</div>
+	<div class="name">Rahman Barika</div>
+	<div class="name">Shaunn Taing</div>
+	<div class="name">Tianna Trujillo</div>
+	<div class="name">William Leduc</div>
+	
+	<div style="clear:both"></div>
+	
+	<h2>Join The Team</h2>
+	
+	<p>Want to learn how to build robots? Want to learn a new skill? Use a power drill for the first time? Maybe you're an artist and you just want to take your passion into a professional setting. There are many roles and jobs that are necessary to maintain a team. Whatever it may be, there's a place for you on FIRST 2557 team. If you'd like to see a more detailed list of our different roles and jobs, click <a href="/Links/list.html" target="_blank">here.</a></p>
+
+	<h2>Awards</h2>
+	<img src="/Images/Awards/awards.jpg" width="950px">
+	<ul>
+		<li>(Seattle) Microsoft Seattle Regional Rookie All-Star '08</li>
+		<li>(Seattle) Microsoft Seattle Regional Winners '10</li>
+		<li>(Seattle) Microsoft Seattle Regional Quality Award '10</li>
+		<li>(Oregon) Autodesk Oregon Regional Team Spirit Award '10</li>
+		<li>(Oregon) Autodesk Oregon Regional Gracious Professionalism Award '11</li>
+		<li>(Seattle) Seattle Cascade Regional Gracious Professionalism Award '11</li>
+		<li>(Seattle) Seattle Cascade Regional Industrial Safety Award '11</li>
+		<li>(Seattle) Seattle Olympic Regional Winners '12</li>
+		<li>(Seattle) Seattle Olympic Regional Industrial Safety Award '12</li>
+		<li>(Seattle) Seattle Olympic Regional Creativity Award '12</li>
+		<li>(Ellensburg) Central Washington Regional Finalist '13</li>
+		<li>(Auburn) Auburn Mountainview District Gracious Professionalism Award '14</li>
+		<li>(Auburn) Auburn Mountainview District Finalist '14</li>
+		<li>(Cheney) Eastern Washington University District Entrepreneur Award '14</li>
+		<li>(Cheney) Eastern Washington University District Finalist '14</li>
+		<li>(Shoreline) Shorewood District Team Spirit Award '14</li>
+	</ul>
+	</div><!--content-->
+</div><!--aside-->
+<div id="footer">
+<div style="float:left">
+<a class="link" href="/index.php">SOTABots</a> <span style="color:#D3D000; font-weight:900;">//</span> 
+</div><div style="float:right">
+<ul>
+<li><a href="https://www.facebook.com/SOTABots2557" target="_blank"><div class="facebook"></div></a></li>
+<li><a href="https://twitter.com/SOTAbots" target="_blank"><div class="twitter"></div></a></li>
+<li><a href="http://www.youtube.com/user/Sotabots2557" target="_blank"><img src="/Images/Footer/youtube.png" style="margin: 0 15px 30px 15px;"/></a></li>
+<li><a href="http://www.flickr.com/photos/sotabots/" target="_blank"><img src="http://l.yimg.com/g/images/flickr_logo_small_white.png" style="background-color:#cccccc; margin: 0 15px 30px 15px; padding:5px;"/></a></li>
+<li><a href="http://sotabots2557.wordpress.com/" target="_blank"><img src="/Images/Footer/wordpresslogo.jpg" width="132px" style="margin:0 15px 30px 15px; background-color:#cccccc; padding: 3px;"/></a></li>
+<li><a href="http://www.usfirst.org/" target="_blank"><img src="/Images/Footer/first.png" style="margin: 0 15px 0 15px;"/></a></li>
+</ul>
+</div>
+</div><!--footer-->
+</div><!--wrapper-->
+</body>
+</html>
