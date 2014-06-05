34e6dd0be4fcc605c3f306adc891f05589bff16d
Kevin-S-H <kevinheckard@gmail.com>
2014-06-05 14:21:38 -0700

    new page for new website

---
+++ sponsors.php
@@ -0,0 +1,107 @@
+<html>
+<head>
+<title>Sponsors</title>
+<link type="text/css" rel="stylesheet" href="/Styles/style.css"></link>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript" src="/Scripts/script.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<body onLoad="init()">
+<div id="wrapper">
+<div id="banner">
+	<a href="/index.php"><img src="/Images/Banner/banner.jpg"/></a><br/>
+</div><!--banner-->
+<div id="aside">
+	<div id="nav">
+		<ul>
+			<li id="NPR"><a href="/Links/pr.php"><img src="/Images/Navigation/pr.png" width="110px"/></a></li>
+			<li id="NBD"><a href="/Links/build.php"><img src="/Images/Navigation/build.png" width="110px"/></a></li>
+			<li id="NPG"><a href="/Links/programming.php"><img src="/Images/Navigation/programming.png" width="110px"/></a></li>
+			<li id="NMD"><a href="/Links/media.php"><img src="/Images/Navigation/media.png" width="110px"/></a></li>
+			<li id="NSP"><a href="/Links/sponsors.php"><img src="/Images/Navigation/sponsors.png" width="110px"/></a></li>
+			<li id="NCT"><a href="/Links/contact us.php"><img src="/Images/Navigation/contact.png" width="110px"/></a></li>
+		</ul>
+	</div><!--nav-->
+	<div id="content">
+<span id="SP">
+		<h1>Sponsors</h1>
+		<h2>THANK YOU TO ALL OUR SPONSORS</h2>
+		<center>
+		<h3 style="background-color:#CCCCFF;">Diamond Sponsors</h3>
+<!--Get photo from internet--><a href="http://elementsofeducation.org/" target="_blank"><img src="http://elementsofeducation.org/wp-content/uploads/2012/09/main-logo.png" style="background-color:#cccccc; padding:5px;" height="50px"/></a>
+		<a href="http://www.usfirst.org/" target="_blank"><img src="/Images/Sponsors/first.png" height="70px"/></a>
+		<a href="http://www.tacoma.k12.wa.us/sites/schools/stewart/Pages/Default.aspx" target="_blank"><img src="/Images/Sponsors/stewart-logo.jpg" height="50px"/></a>
+		<a href="http://www.boeing.com/" target="_blank"><img src="/Images/Sponsors/boeing.png" height="50px" width="160px"/></a>
+		<a href="http://www.deliveryexpressinc.com/" target="_blank"><img src="/Images/Sponsors/deliveryexpress.png" height="50px"/></a>
+		<a href="http://www.zumar.com/" target="_blank"><img src="/Images/Sponsors/zumar.gif" height="50px"/></a>
+		<a href="http://titaniumhotforming.com/" target="_blank"><img src="/Images/Sponsors/CarlsonFormetec.png" height="50px"/></a>
+		
+		<h3 style="background-color:#CCCCCC;">Platinum Sponsors</h3>
+		<a href="http://www.pacificmetal.com/" target="_blank"><img src="/Images/Sponsors/pacificmetal.jpg" height="50px"/></a>
+		<a href="http://www.k12.wa.us/" target="_blank"><img src="/Images/Sponsors/OSPI.png"/></a>
+		<a href="http://www.tacoma.k12.wa.us/Pages/default.aspx" target="_blank"><img src="/Images/Sponsors/tpslogo.png" height="50px" width="125px"/></a>
+		<a href="http://www.alaskaair.com/" target="_blank"><img src="/Images/Sponsors/alaskaairlines.png" height="50px width="200px/></a>
+		<a href="http://www.nwsteel.net/" target="_blank"><img src="/Images/Sponsors/NWsteel.gif" height="50px"/></a>
+		<a href="http://www.blueorigin.com/" target="_blank"><img src="/Images/Sponsors/blueoriginnew.png" height="60px"/></a>
+		
+		<h3 style="background-color:#FFD700;">Gold Sponsors</h3>
+		<a href="http://www.schneidersimpson.com/" target="_blank"><img src="/Images/Sponsors/schneidersimpson.gif" height="50px"/></a>
+		<h3 style="background-color:#C9C0BB;">Silver Sponsors</h3>
+		<a href="http://www.multicare.org/" target="_blank"><img src="/Images/Sponsors/multicare.gif" height="50px"/></a>
+		<a href="http://www.f5.com/" target="_blank"><img src="/Images/Sponsors/f5.png" height="50px"/></a>
+		
+		<h3 style="background-color:#CD7F32;">Bronze Sponsors</h3>
+		<a href="http://www.globalnavigationsciences.com" target="_blank"><img src="/Images/Sponsors/gnslogo.png" height="60px" width="80px"></a>
+		<a href="http://www.pointruston.com/" target="_blank"><img src="/Images/Sponsors/Point Ruston.jpg" height="60px" width="60px"></a>
+		
+		</center>
+		<h2>Looking For A Partner?</h2>
+		<p>The SOTABots are a robotics team involved in the FIRST Robotics Competition. FIRST (For Inspiration and Recognition of Science and Technology) is a mentor based program. It was founded by renowned inventor Dean Kamen with the goal of transforming our culture by creating a world where science and technology are celebrated, and where young people begin to realize their dream of becoming science and technology leaders.</p>
+		<p>SOTABots was started at the Tacoma School of the Arts in 2008 and has since expanded to include team members from not only the Tacoma School of the Arts and their sister school SAMI (Science and Math Institute), but also from Lincoln High School and Stadium High School.</p>
+		<p>SOTABots personifies Dean Kamen's vision by mentoring other FIRST teams and giving robotics presentations at local middle schools to get possible future team members excited about robotics and engineering. SOTABots, like all FIRST teams, is not just about building a robot and competing. It is about the life skills students can acquire through the hard work and dedication of creating something while being an integral part of a team.</p>
+		<p> Thanks in no small part to our gracious sponsors we have been able to accomplish great things since our inception. In 2008 we achieved the "Rookie of the Year Award" and in 2010 we won the Seattle Microsoft regional competition. However our performance in the tournaments is not our sole focus. SOTABots is dedicated to creating well-rounded individuals who reach out to help other teams and the local community. This has been acknowledged through other honors we have received such the Chrysler Team Spirit Award, The Industrial Safety Award, the Motorola Quality Award, and the Gracious Professionalism Award.</p>
+		<p>As a sponsor, your logo will be seen throughout Western Washington and points beyond. Our team apparel is worn not only for competition but throughout the year by students and mentors alike as a badge of pride. At the world competition in St. Louis, April 2014, we were one of 400 teams that qualified to compete. Every year, this organization is growing so we will need sponsors to compete to the best of our abilities. Your tax-deductible donation will not only support the SOTABots, but provide further advertising exposure for your business as well. </p>
+		<p>Our team, the life skills it builds, and the honors we've received would never have been possible without the support of sponsors. We would be proud to have you as a sponsor. Any and all sponsorship levels are greatly appreciated. </p>
+
+		<h2>Businesses and organizations can sponsor SOTABots at the following levels:</h2>
+	
+		<h3 style="background-color:#CCCCFF;">Diamond ($3,000 and up)</h3>
+		<p>Your company name will be featured in our official name, in addition to the platinum benefits.</p>
+
+		<h3 style="background-color:#CCCCCC;">Platinum ($2,000-$2,999)</h3>
+		<p>Your company logo will be featured on our robot, in addition to the gold benefits.</p>
+
+		<h3 style="background-color:#FFD700;">Gold ($1,000-$1,999)</h3>
+		<p>Your company logo will be featured in our pit area at competitions, in addition to the silver benefits.</p>
+
+		<h3 style="background-color:#C9C0BB;">Silver ($750-$999)</h3>
+		<p>Your company logo will be featured on our team t-shirts, in addition to the bronze benefits.</p>
+
+		<h3 style="background-color:#CD7F32;">Bronze ($500-$749)</h3>
+		<p>Your company logo will be featured on our brochure and on our website, in addition to the copper benefits.</p>
+
+		<h3 style="background-color:#AD6F69;">Copper ($100-$499)</h3>
+		<p>You will receive a thank you letter and a picture of the completed robot at the end of the competition season.</p>
+
+		<p>***Donations of services, food, materials, tools, and other items are gladly accepted, and will be considered at fair market value.</p>
+	</span><!--Sponsors-->
+	</div><!--content-->
+</div><!--aside-->
+<div id="footer">
+<div style="float:left">
+<a class="link" href="/index.php">SOTABots</a> <span style="color:#D3D000; font-weight:900;">//</span> 
+</div><div style="float:right">
+<ul>
+<li><a href="https://www.facebook.com/SOTABots2557" target="_blank"><div class="facebook"></div></a></li>
+<li><a href="https://twitter.com/SOTAbots" target="_blank"><div class="twitter"></div></a></li>
+<li><a href="http://www.youtube.com/user/Sotabots2557" target="_blank"><img src="/Images/Footer/youtube.png" style="margin: 0 15px 30px 15px;"/></a></li>
+<li><a href="http://www.flickr.com/photos/sotabots/" target="_blank"><img src="http://l.yimg.com/g/images/flickr_logo_small_white.png" style="background-color:#cccccc; margin: 0 15px 30px 15px; padding:5px;"/></a></li>
+<li><a href="http://sotabots2557.wordpress.com/" target="_blank"><img src="/Images/Footer/wordpresslogo.jpg" width="132px" style="margin:0 15px 30px 15px; background-color:#cccccc; padding: 3px;"/></a></li>
+<li><a href="http://www.usfirst.org/" target="_blank"><img src="/Images/Footer/first.png" style="margin: 0 15px 0 15px;"/></a></li>
+</ul>
+</div>
+</div><!--footer-->
+</div><!--wrapper-->
+</body>
+</html>
