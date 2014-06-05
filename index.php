83c0f47e0f8db0cc59f3ccce391cea575bc43963
Kevin-S-H <kevinheckard@gmail.com>
2014-06-05 14:17:27 -0700

    Update and rename index.html to index.php
    made fro new website

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="Styles/style.css"></link>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript" src="Scripts/script.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<body onLoad="init()">
+<div id="wrapper">
+<div id="banner">
+	<a href="index.html"><img src="Images/Banner/banner.jpg"/></a><br/></div>
+	<div id="nav">
+		<ul>
+			<li id="NPR"><a href="Links/pr.php"><img src="Images/Navigation/pr.png" width="110px"/></a></li>
+			<li id="NBD"><a href="Links/build.php"><img src="Images/Navigation/build.png" width="110px"/></a></li>
+			<li id="NPG"><a href="Links/programming.php"><img src="Images/Navigation/programming.png" width="110px"/></a></li>
+			<li id="NMD"><a href="Links/media.php"><img src="Images/Navigation/media.png" width="110px"/></a></li>
+			<li id="NSP"><a href="Links/sponsors.php"><img src="Images/Navigation/sponsors.png" width="110px"/></a></li>
+			<li id="NCT"><a href="Links/contact us.php"><img src="Images/Navigation/contact.png" width="110px"/></a></li>
+		</ul>
+	</div><!--nav-->
+	<div id="banner">
+	<div id="counter">
+	</div>
+	<div id="object">
+	</div><!--object-->
+</div><!--banner-->
+<div id="aside">
+	
+	<div id="content">
+	
+	<span id="whatIsFirst">
+	<h1>What is FIRST?</h1>
+	<p>FIRST (For the Inspiration and Recognition of Science and Technology) is a program founded by Dean Kamen to inspire students to be leaders in science and technology, innovation, and foster well-rounded life capabilities including self-confidence, communication, and leadership in students as well as build science, engineering, technology and problem solving skills. To learn more about the FIRST organization, click <a href="http://www.usfirst.org/" target="_blank">here</a>.</p>
+	</span><!--whatIsFirst-->
+	<h1>Current News</h1>
+	<h2>Working Hard</h2>
+	<p><center><img src="Images/Current News/working hard during microterm.jpg"/></center></p>
+	<p>Micro-term started this Monday and we have been working hard to get ready for our summer outreach. We have started planning lessons for our monthly FLL Camps and finding great resources for teaching STEM to elementary schoolers. We are also planning visits to our current sponsors and an FLL workshop before the FLL kick-off. The build team is working on a new drive-train prototype.
+Today we also had our first shop fire drill. It was unexpected and a really great way to learn about procedures in the event of an emergency at the school. We are very prepared now!</p>
+	<h2>St. Louis</h2>
+	<p>We were placed in the Archimedes Division in which we ranked 39<sup>th</sup> with 6 wins and 4 losses. We were chosen by the 2<sup>nd</sup> seed alliance with the captain team being FIRST Team 2907 Lion Robotics. The other teams allied with us were FIRST Team 3393 Horns of Havoc and FIRST Team 4911 CyberKnights. Our all Northwest region alliance pushed our way to the semi-finals and lost to the 3<sup>rd</sup> seed alliance.</p>
+	<h2>Packing for St. Louis</h2>
+	<p><center><img src="Images/Current News/st.louispacking.jpg"/></center></p>
+	<p>In the Portland competition, we were one of twenty-four teams that qualified to go to nationals in St. Louis. We are now packing up with team 3393, Horns of Havoc, so we can compete to the best of our ability. We are excited to compete with 398 teams from all over the world. Our team will be competing on Archimedes field.</p>
+	</div><!--content-->
+</div><!--aside-->
+<div id="footer">
+<div style="float:left">
+<a class="link" href="index.html">SOTABots</a> <span style="color:#D3D000; font-weight:900;">//</span> 
+</div><div style="float:right">
+<ul>
+<li><a href="https://www.facebook.com/SOTABots2557" target="_blank"><div class="facebook"></div></a></li>
+<li><a href="https://twitter.com/SOTAbots" target="_blank"><div class="twitter"></div></a></li>
+<li><a href="http://www.youtube.com/user/Sotabots2557" target="_blank"><img src="/Images/Footer/youtube.png" style="margin: 0 15px 30px 15px;"/></a></li>
+<li><a href="http://www.flickr.com/photos/sotabots/" target="_blank"><img src="http://l.yimg.com/g/images/flickr_logo_small_white.png" style="background-color:#cccccc; margin: 0 15px 30px 15px; padding:5px;"/></a></li>
+<li><a href="http://sotabots2557.wordpress.com/" target="_blank"><img src="/Images/Footer/wordpresslogo.jpg" width="132px" style="margin:0 15px 30px 15px; background-color:#cccccc; padding: 3px;"/></a></li>
+<li><a href="http://www.usfirst.org/" target="_blank"><img src="/Images/Footer/first.png" style="margin: 0 15px 0 15px;"/></a></li></ul>
+</div>
+</div><!--footer-->
+</div><!--wrapper-->
+</body>
+</html>
