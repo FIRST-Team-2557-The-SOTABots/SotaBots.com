24fc573afba060e6401441bfbaa8cdc4f51bdbfc
Kevin-S-H <kevinheckard@gmail.com>
2014-06-05 14:19:57 -0700

    new page for new website

---
+++ media.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+<title>Media</title>
+<link type="text/css" rel="stylesheet" href="/Styles/style.css"></link>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript" src="/Scripts/script.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<body onLoad="init()">
+<div id="wrapper">
+<div id="banner">
+	<a href="/index.php"><img src="/Images/Banner/banner.jpg"/></a><br/>
+</div><!--banner-->
+<div id="aside">
+	<div id="nav">
+		<ul>
+			<li id="NPR"><a href="/Links/pr.php"><img src="/Images/Navigation/pr.png" width="110px"/></a></li>
+			<li id="NBD"><a href="/Links/build.php"><img src="/Images/Navigation/build.png" width="110px"/></a></li>
+			<li id="NPG"><a href="/Links/programming.php"><img src="/Images/Navigation/programming.png" width="110px"/></a></li>
+			<li id="NMD"><a href="/Links/media.php"><img src="/Images/Navigation/media.png" width="110px"/></a></li>
+			<li id="NSP"><a href="/Links/sponsors.php"><img src="/Images/Navigation/sponsors.png" width="110px"/></a></li>
+			<li id="NCT"><a href="/Links/contact us.php"><img src="/Images/Navigation/contact.png" width="110px"/></a></li>
+		</ul>
+	</div><!--nav-->
+	<div id="content">
+<h1 id="MD">Media</h1>
+	<center>
+	<p>Videos can be found on the <a href="http://www.youtube.com/user/Sotabots2557" target="_blank">SotaBots Youtube Page</a>.</p>
+	<font style="color:#aaaaaa;">Click to enlarge. Click again to return size to normal.</font><br/>
+<center>
+   <h2>Outreach</h2>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/sci-fiedance.jpg" width="100%"/></div>
+    	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/banner5.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/banner1.jpg" width="100%"/></div>
+    	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/IMG_9219.jpg" width="100%"/></div>
+    	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/cheesyflyer.jpg" width="100%"/></div>
+    	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/IMG_7907.jpg" width="100%"/></div>
+        <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/alex_matrybridge.jpg" width="100%"></div>
+        <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Outreach/carson_marybridge.jpg" width="100%"></div>
+<div style="clear:both"></div>
+	
+       <h2>Team</h2>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/humanplayers.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/justiceandeli.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/2557teamback.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/2557teamfront.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/dancebreak.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/IMG_9161.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/banner2.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/IMG_6645.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/banner4.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/banner3.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/Team-Photo-Seattle.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/team2557.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/pit.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/friendly.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/tra.jpg" width="100%"/></div>
+    <div class="image" onClick="imageSize(this)"><img src="/Images/Media/Team/ymca.jpg" width="100%"/></div>
+<div style="clear:both"></div> 
+    
+    <h2>Robot</h2>
+    <center>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/banner6.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/banner7.jpg" width="100%"/></div>	
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/IMG_8300.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/IMG_8347.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/robot.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/robot2.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/robot3.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/robot4.jpg" width="100%"/></div>
+	<div class="image" onClick="imageSize(this)"><img src="/Images/Media/Robot/robot5.jpg" width="100%"/></div>
+	</center>
+<div style="clear:both"></div>
+    
+    <h2>Team Resources</h2>
+    
+	</div><!--content-->
+</div><!--aside-->
+<div id="footer">
+<div style="float:left">
+<a class="link" href="/index.php">SOTABots</a> <span style="color:#D3D000; font-weight:900;">//</span>  
+</div><div style="float:right">
+<ul>
+<li><a href="https://www.facebook.com/SOTABots2557" target="_blank"><div class="facebook"></div></a></li>
+<li><a href="https://twitter.com/SOTAbots" target="_blank"><div class="twitter"></div></a></li>
+<li><a href="http://www.youtube.com/user/Sotabots2557" target="_blank"><img src="/Images/Footer/youtube.png" style="margin: 0 15px 30px 15px;"/></a></li>
+<li><a href="http://www.flickr.com/photos/sotabots/" target="_blank"><img src="http://l.yimg.com/g/images/flickr_logo_small_white.png" style="background-color:#cccccc; margin: 0 15px 30px 15px; padding:5px;"/></a></li>
+<li><a href="http://sotabots2557.wordpress.com/" target="_blank"><img src="/Images/Footer/wordpresslogo.jpg" width="132px" style="margin:0 15px 30px 15px; background-color:#cccccc; padding: 3px;"/></a></li>
+<li><a href="http://www.usfirst.org/" target="_blank"><img src="/Images/Footer/first.png" style="margin: 0 15px 0 15px;"/></a></li>
+</ul>
+</div>
+</div><!--footer-->
+</div><!--wrapper-->
+</body>
+</html>
